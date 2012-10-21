6633a5f81090ad6d5fd246396aea5063ca8d99a3
Craig Edwards <craig@hae.com.au>
2012-10-22 08:37:13 +1100

    Initial checkin for http://www.blackdogfoundry.com/blog/creating-an-xcode4-plugin/

---
+++ BDXcodePluginDemo.h
@@ -0,0 +1,13 @@
+//
+//  BDXcodePluginDemo.h
+//  BDXcodePluginDemo
+//
+//  Created by Craig Edwards on 22/10/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BDXcodePluginDemo : NSObject
+
+@end
---
+++ BDXcodePluginDemo.m
@@ -0,0 +1,17 @@
+//
+//  BDXcodePluginDemo.m
+//  BDXcodePluginDemo
+//
+//  Created by Craig Edwards on 22/10/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import "BDXcodePluginDemo.h"
+
+@implementation BDXcodePluginDemo
+
++(void)pluginDidLoad:(NSBundle *)plugin {
+	NSLog(@"This is our first Xcode plugin!");
+}
+
+@end
